1f25bd30c0a6c89803dcffe155b9aed58db058c3
RLiteM <mvicentem6@miumg.edu.gt>
2024-05-07 23:25:15 -0600

    Segundo Comit

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main{
     public static void main(String[] args){
         System.out.print("xd");
         System.out.println("Prueba 2");
+        System.out.println("Segundo Prueba");
     }
 }
